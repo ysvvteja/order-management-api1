4c6db3394ba1642739864ff2f0c0debc17830b63
ysvvteja <ysvvteja@gmail.com>
2025-11-13 23:16:50 +0530

    Duplicate files are existing because the commit didnot work porperly

--- src/main/java/repository/OrderRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, String> {}
